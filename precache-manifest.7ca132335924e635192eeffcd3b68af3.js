2778c2bd0eab3fc94fb22ded7041d9fd52e9a13c
jiebao <908822571@qq.com>
2021-01-19 17:29:21 +0800

    deploy

---
+++ precache-manifest.7ca132335924e635192eeffcd3b68af3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75180a72bdad6baa4ca",
+    "url": "/morney-1-website/css/app.a84fd154.css"
+  },
+  {
+    "revision": "48731a9a8efcf5fb7ee9bbd2194635b0",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "d75180a72bdad6baa4ca",
+    "url": "/morney-1-website/js/app.c52f1460.js"
+  },
+  {
+    "revision": "36d41d682977336dc3fd",
+    "url": "/morney-1-website/js/chunk-vendors.ab3c2af2.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "c239d3928408a611ca413bc9f1bebc67",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
